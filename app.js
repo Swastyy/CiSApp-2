a513b55d694656135385897a02117c006d80ae7c
Swastyy <gupta.swastika521@gmail.com>
2021-05-07 13:58:19 +0530

    jsk

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+//const home = require("./home.ejs");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render(__dirname+"/views/home.ejs");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
